52d7c073ac991b69826c7fb5530ca74febc91af2
Azam <mohammedazam651@gmail.com>
2022-10-18 13:08:19 +0530

    live seconds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:timer_builder/timer_builder.dart';
+import './ui/styles.dart';
 
 void main(List<String> args) => runApp(const MyApp());
 
@@ -10,19 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final hr1 = DateTime.now().hour;
-  final date = DateTime.now().day;
-  final month = DateTime.now().month;
-  final year = DateTime.now().year;
-  final min = DateTime.now().minute;
-  final sec = DateTime.now().second;
-  double percentDay =
-      ((DateTime.now().hour / 24) * 100).toDouble().roundToDouble();
-  double percentMonth =
-      ((DateTime.now().day / 30) * 100).toDouble().roundToDouble();
-  double percentYear =
-      ((DateTime.now().month / 12) * 100).toDouble().roundToDouble();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,17 +20,52 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Life is short'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Time Now - $hr1 : $min : $sec'),
-              Text('$date - $month - $year'),
-              Text('$percentDay % of day has gone'),
-              Text('$percentMonth % of month has completed'),
-              Text('$percentYear % of year has completed'),
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TimerBuilder.periodic(const Duration(seconds: 1),
+                builder: (context) {
+              final hr1 = DateTime.now().hour;
+              final date = DateTime.now().day;
+              final month = DateTime.now().month;
+              final year = DateTime.now().year;
+              final min = DateTime.now().minute;
+              final sec = DateTime.now().second;
+              double percentDay =
+                  ((DateTime.now().hour / 24) * 100).toDouble().roundToDouble();
+              double percentMonth =
+                  ((DateTime.now().day / 30) * 100).toDouble().roundToDouble();
+              double percentYear = ((DateTime.now().month / 12) * 100)
+                  .toDouble()
+                  .roundToDouble();
+              return Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Time Now - $hr1 : $min : $sec',
+                      style: Styles.mainText,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(26.0),
+                      child: Text(
+                        '$date - $month - $year',
+                        style: Styles.mainText,
+                      ),
+                    ),
+                    Text(
+                      '$percentDay % of day has gone',
+                    ),
+                    Text(
+                      '$percentMonth % of month has completed',
+                    ),
+                    Text(
+                      '$percentYear % of year has completed',
+                    ),
+                  ],
+                ),
+              );
+            })
+          ],
         ),
       ),
     );
---
+++ lib/ui/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static TextStyle mainText = const TextStyle(
+    color: Colors.black,
+    fontSize: 32.0,
+    fontWeight: FontWeight.bold,
+  );
+}
